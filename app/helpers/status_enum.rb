bb022f4476771eeb77d45bc254e86694739c9e39
Karina Almeida <s.karinaalmeida@gmail.com>
2023-09-19 15:56:49 -0300

    custom status enum

--- app/helpers/status_enum.rb
+++ app/helpers/status_enum.rb
@@ -6,14 +6,4 @@ class StatusEnum
     'trash' => 'trash',
     'published' => 'published'
   }.freeze
-
-  def self.options
-    MAPPING.keys
-  end
-
-  def self.error_message(value)
-    return if value.include?(options)
-
-    raise ArgumentError, "must be one of: #{options.join(', ')}"
-  end
 end
