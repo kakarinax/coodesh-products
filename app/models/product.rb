177e102d1d39825a67177b8a8cfeb05acd6bbd16
Karina Almeida <s.karinaalmeida@gmail.com>
2023-09-19 14:09:29 -0300

    add product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,8 +3,24 @@ class Product
   include Mongoid::Enum
   include Mongoid::Timestamps
 
-  enum :status, %i[draft trash published], null: false
+  enum status: %i[draft published trash], null: false
   field :imported_t, type: DateTime, null: false
   field :code, type: Integer
   field :url, type: String
+  field :creator, type: String
+  field :product_name, type: String
+  field :quantity, type: String
+  field :brands, type: String
+  field :categories, type: String
+  field :labels, type: String
+  field :cities, type: String
+  field :purchase_places, type: String
+  field :stores, type: String
+  field :ingredients_text, type: String
+  field :traces, type: String
+  field :serving_size, type: String
+  field :serving_quantity, type: Float
+  field :nutriscore_score, type: Integer
+  field :main_category, type: String
+  field :image_url, type: String
 end
