0847cbd137ee0233bcc1826905a6c79822d82a5d
Karina Almeida <s.karinaalmeida@gmail.com>
2023-09-22 00:13:20 -0300

    schedule yml updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      get '/' => 'api_status#index'
+      put '/products/:code' => 'products#update'
+      get '/products/:code' => 'products#show'
+      get '/products' => 'products#index'
+      delete '/products/:code' => 'products#delete'
+    end
+  end
 end
