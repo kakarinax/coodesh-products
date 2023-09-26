25f45f75d13af18e1990dcb01b591d43614bc9ae
Karina Almeida <s.karinaalmeida@gmail.com>
2023-09-26 19:09:49 -0300

    log history and model

--- app/models/import_history.rb
+++ app/models/import_history.rb
@@ -5,8 +5,9 @@ class ImportHistory
   field :imported_at, type: DateTime
   field :status, type: String
   field :file_name, type: String
-  field :imported_count, type: Integer
   field :error_message, type: String
+  field :cron_execution_time, type: DateTime
+  field :memory_usage, type: String
 
   embeds_many :products, inverse_of: :import_history
 end
--- app/services/import_services/history_logger.rb
+++ app/services/import_services/history_logger.rb
@@ -1,14 +1,17 @@
 module ImportServices
   class HistoryLogger
-    def log_success(file_name)
+    def log_success(file_name, cron_execution_time)
       ImportHistory.create!(imported_at: DateTime.now,
                             status: 'success',
                             file_name:,
-                            imported_count: Product.count)
+                            cron_execution_time:)
     end
 
     def log_failure(file_name)
-      ImportHistory.create!(imported_at: DateTime.now, status: 'failure', file_name:)
+      ImportHistory.create!(imported_at: DateTime.now,
+                            status: 'failure', file_name:,
+                            cron_execution_time:,
+                            error_message: 'Error importing file')
     end
   end
 end
